9e4f3392bd3051271608edc60b32d0db99cad54e
drsean3 <andyvelezr@gmail.com>
2019-07-16 11:08:25 -0500

    Implementacion de dbcontext

---
+++ GymSoftApplication/GymSoftApplication/Models/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GymSoftApplication.Models
+{
+    public class UserContext : DbContext
+
+    {
+        public UserContext()
+            :base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<UserModel> UserModels { get; set; }
+    }
+}
