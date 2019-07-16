0800e3686e7b5fb8852239902d05d7e7062bfd71
drsean3 <andyvelezr@gmail.com>
2019-07-16 10:46:15 -0500

    Template Project

---
+++ GymSoftApplication/GymSoftApplication/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GymSoftApplication.Models
+{
+    public class UserModel
+    {
+        public int idUser { get; set; }
+        public string userName { get; set; }
+    }
+}
---
+++ GymSoftApplication/GymSoftApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymSoftApplication.Startup))]
+namespace GymSoftApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
