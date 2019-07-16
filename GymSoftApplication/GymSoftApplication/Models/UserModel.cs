696ae1ef8524cfe09424462afc771f6b77bff315
drsean3 <andyvelezr@gmail.com>
2019-07-16 15:55:02 -0500

    Implementación enable migrations

--- GymSoftApplication/GymSoftApplication/Models/UserModel.cs
+++ GymSoftApplication/GymSoftApplication/Models/UserModel.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace GymSoftApplication.Models
 {
     public class UserModel
     {
-        public int idUser { get; set; }
+        [Key]
+        public int idUser { get; set; }     
+        [Required]  
+        [StringLength(20)] 
         public string userName { get; set; }
+        [Required]
+        [StringLength(25)]
         public string userType { get; set; }
+        [Required]
+        public DateTime visitCreation { get; set; }
     }
 }
